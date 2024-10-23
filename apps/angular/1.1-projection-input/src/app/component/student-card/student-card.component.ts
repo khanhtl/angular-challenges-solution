3e81fd2fe27462175df94264a91d3a6b3990a31a
tlkhanh <longkhanhta@gmail.com>
2024-10-23 23:19:45 +0700

    feat: angular projection solution 01 using input

---
+++ apps/angular/1.1-projection-input/src/app/component/student-card/student-card.component.ts
@@ -0,0 +1,59 @@
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { takeUntilDestroyed, toSignal } from '@angular/core/rxjs-interop';
+import {
+  FakeHttpService,
+  randStudent,
+} from '../../data-access/fake-http.service';
+import { StudentStore } from '../../data-access/student.store';
+import { Student } from '../../model/student.model';
+import { CardComponent } from '../../ui/card/card.component';
+import { ListItemComponent } from '../../ui/list-item/list-item.component';
+
+@Component({
+  selector: 'app-student-card',
+  template: `
+    <ng-template #itemTemplate let-item>
+      <app-list-item (deleteItem)="handleDeleteItem(item)">
+        {{ item.firstName }}
+      </app-list-item>
+    </ng-template>
+    <app-card
+      class="bg-light-green"
+      [list]="students()"
+      [itemTemplate]="itemTemplate"
+      (addNewItem)="handleAddItem()">
+      <img src="assets/img/student.webp" width="200px" />
+    </app-card>
+  `,
+  standalone: true,
+  styles: [
+    `
+      :host {
+        .bg-light-green {
+          background-color: rgba(0, 250, 0, 0.1);
+        }
+      }
+    `,
+  ],
+  imports: [CardComponent, ListItemComponent],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class StudentCardComponent {
+  #http = inject(FakeHttpService);
+  #store = inject(StudentStore);
+
+  students = toSignal(this.#store.students$);
+
+  constructor() {
+    this.#http.fetchStudents$
+      .pipe(takeUntilDestroyed())
+      .subscribe((s) => this.#store.addAll(s));
+  }
+  handleDeleteItem(item: Student) {
+    this.#store.deleteOne(item.id);
+  }
+
+  handleAddItem() {
+    this.#store.addOne(randStudent());
+  }
+}
